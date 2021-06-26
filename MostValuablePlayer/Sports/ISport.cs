2bd1822ab93be348085ecea15343761fb83f000e
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:10:29 +0300

    Created ISport.cs interface

---
+++ MostValuablePlayer/Sports/ISport.cs
@@ -0,0 +1,19 @@
+﻿using MostValuablePlayer.Core;
+using MostValuablePlayer.Stats;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MostValuablePlayer.Sports
+{
+    interface ISport
+    {
+        /// <summary>
+        /// Creates PlayerStats of a match
+        /// </summary>
+        /// <param name="match"></param>
+        /// <param name="rawPlayerInfo"></param>
+        /// <returns>PlayerStats of a match</returns>
+        PlayerStats GetStats(Match match, RawInfo rawPlayerInfo);
+    }
+}

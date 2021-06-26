e5d657faad841eb5b29a105d99007c51c74de2f5
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:07:18 +0300

    Created IPlayerPosition.cs interface

--- MostValuablePlayer/PlayerPositions/IPlayerPosition.cs
+++ MostValuablePlayer/PlayerPositions/IPlayerPosition.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using MostValuablePlayer.Stats;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MostValuablePlayer.PlayerPositions
 {
+    /// <summary>
+    /// Interface of player positions in sports
+    /// </summary>
     interface IPlayerPosition
     {
+        /// <summary>
+        /// Calculates player score in match based on rating points defined per sport
+        /// </summary>
+        /// <param name="stats">detailed match scores statistics</param>
+        /// <returns>total score calculated using ratings of sport's position</returns>
+        int CalculateScore(Dictionary<string, int> stats);
     }
 }

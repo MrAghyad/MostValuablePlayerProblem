093efcbd0d1e10ea26e976192a40554139e2b728
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:07:46 +0300

    Created BasketballPosition.cs

--- MostValuablePlayer/PlayerPositions/Basketball/BasketballPosition.cs
+++ MostValuablePlayer/PlayerPositions/Basketball/BasketballPosition.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using MostValuablePlayer.Stats;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MostValuablePlayer.PlayerPositions.Basketball
 {
-    class BasketballPosition : IPlayerPosition
+    /// <summary>
+    /// Abstract class that represents basketball positions
+    /// </summary>
+    abstract class BasketballPosition : IPlayerPosition
     {
+        /// <summary>
+        /// Calculates player score in match based on rating points defined per basketball position
+        /// </summary>
+        /// <param name="stats">detailed match scores statistics</param>
+        /// <returns>total score calculated using ratings of basketball position</returns>
+        public abstract int CalculateScore(Dictionary<string, int> stats);
         
     }
 }

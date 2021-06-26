996fa679fe26bd72baa5362ffa891b1aee8b7cb7
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:10:58 +0300

    Created Handball.cs

--- MostValuablePlayer/Sports/Handball.cs
+++ MostValuablePlayer/Sports/Handball.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using MostValuablePlayer.Core;
+using MostValuablePlayer.Stats;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MostValuablePlayer.Sports
 {
-    class Handball : Sport
+    /// <summary>
+    /// A class that represents handball sport that implements ISport
+    /// </summary>
+    class Handball : ISport
     {
+        /// <summary>
+        /// Creates PlayerStats of a match
+        /// </summary>
+        /// <param name="match"></param>
+        /// <param name="rawPlayerInfo"></param>
+        /// <returns>HandballStats of a match</returns>
+        public PlayerStats GetStats(Match match, RawInfo rawPlayerInfo)
+        {
+            return new HandballStats(match, rawPlayerInfo);
+        }
     }
 }

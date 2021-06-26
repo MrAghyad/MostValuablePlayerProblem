1550c21bf4be10db5dea09535dd2ced9f75feef1
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:10:44 +0300

    Created Basketball.cs

--- MostValuablePlayer/Sports/Basketball.cs
+++ MostValuablePlayer/Sports/Basketball.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using MostValuablePlayer.Core;
+using MostValuablePlayer.Stats;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MostValuablePlayer.Sports
 {
-    class Basketball : Sport
+    /// <summary>
+    /// A class that represents basketball sport that implements ISport
+    /// </summary>
+    class Basketball : ISport
     {
+        /// <summary>
+        /// Creates PlayerStats of a match
+        /// </summary>
+        /// <param name="match"></param>
+        /// <param name="rawPlayerInfo"></param>
+        /// <returns>BasketballStats of a match</returns>
+        public PlayerStats GetStats(Match match, RawInfo rawPlayerInfo)
+        {
+            return new BasketballStats(match, rawPlayerInfo);
+        }
     }
 }

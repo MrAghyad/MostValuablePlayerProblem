fb926e3985149849f737483958388815e15a86aa
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:09:19 +0300

    Created FieldPlayer.cs

--- MostValuablePlayer/PlayerPositions/Handball/FieldPlayer.cs
+++ MostValuablePlayer/PlayerPositions/Handball/FieldPlayer.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using MostValuablePlayer.Stats;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MostValuablePlayer.PlayerPositions.Handball
 {
+    /// <summary>
+    /// A class representing FieldPlayer handball position
+    /// </summary>
     class FieldPlayer : HandballPosition
     {
+        /// <summary>
+        /// Calculates player score in match based on rating points defined for FieldPlayer
+        /// </summary>
+        /// <param name="stats">detailed match scores statistics</param>
+        /// <returns>total score calculated using ratings of FieldPlayer position</returns>
+        public override int CalculateScore(Dictionary<string, int> stats)
+        {
+
+            return (stats[HandballScoreStats.InitialRatingPoints.ToString()] * 20)
+                + (stats[HandballScoreStats.GoalMade.ToString()] * 1)
+                + (stats[HandballScoreStats.GoalReceived.ToString()] * -1);
+
+        }
     }
 }

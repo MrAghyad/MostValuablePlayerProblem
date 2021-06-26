d6f16a3a38f3394ceb61e74e22c6d742ca804e6e
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:08:29 +0300

    Created Center.cs

--- MostValuablePlayer/PlayerPositions/Basketball/Center.cs
+++ MostValuablePlayer/PlayerPositions/Basketball/Center.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using MostValuablePlayer.Stats;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MostValuablePlayer.PlayerPositions.Basketball
 {
+    /// <summary>
+    /// A class representing Center BasketballPosition
+    /// </summary>
     class Center : BasketballPosition
     {
+        /// <summary>
+        /// Calculates player score in match based on rating points defined for Center
+        /// </summary>
+        /// <param name="stats">detailed match scores statistics</param>
+        /// <returns>total score calculated using ratings of Center position</returns>
+        public override int CalculateScore(Dictionary<string, int> stats)
+        {
+            return (stats[BasketballScoreStats.ScoredPoint.ToString()] * 2)
+                + (stats[BasketballScoreStats.Rebound.ToString()] * 1)
+                + (stats[BasketballScoreStats.Assist.ToString()] * 3);
+        }
     }
 }

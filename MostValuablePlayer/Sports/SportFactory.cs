a66d6b08738607d233d0652e63d5802141a55897
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:11:18 +0300

    Created SportFactory.cs

---
+++ MostValuablePlayer/Sports/SportFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MostValuablePlayer.Sports
+{
+    /// <summary>
+    /// A factory that creates ISport types
+    /// </summary>
+    class SportFactory
+    {
+        /// <summary>
+        /// Creates an instance of class that implements ISport using a provided sportName
+        /// </summary>
+        /// <param name="sportName">string containing sport name</param>
+        /// <returns>ISport class instance</returns>
+        public ISport GetSport(string sportName)
+        {
+            if(sportName.Equals("BASKETBALL", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Basketball();
+            }
+
+            if (sportName.Equals("HANDBALL", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Handball();
+            }
+
+            return null;
+        }
+    }
+}

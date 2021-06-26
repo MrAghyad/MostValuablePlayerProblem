e6e1e850ecfb7c790281537cbb7ff7f8e6e485d3
MrAghyad <theriddler_214@hotmail.com>
2021-06-26 23:09:37 +0300

    Created HandballPosition.cs

--- MostValuablePlayer/PlayerPositions/Handball/HandballPosition.cs
+++ MostValuablePlayer/PlayerPositions/Handball/HandballPosition.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace MostValuablePlayer.PlayerPositions.Handball
 {
-    class HandballPosition : IPlayerPosition
+    /// <summary>
+    /// Abstract class that represents handball positions
+    /// </summary>
+    abstract class HandballPosition : IPlayerPosition
     {
+        /// <summary>
+        /// Calculates player score in match based on rating points defined per handball position
+        /// </summary>
+        /// <param name="stats">detailed match scores statistics</param>
+        /// <returns>total score calculated using ratings of handball position</returns>
+        public abstract int CalculateScore(Dictionary<string, int> stats);
     }
 }
